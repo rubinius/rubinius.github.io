7ddd394c1cca4984171d1548c831f5693924b695
Brian Shirai <brixen@gmail.com>
2025-11-09 15:59:22 -0800

    Add social icons and improve theme switching.

--- src/content/config.ts
+++ src/content/config.ts
@@ -22,28 +22,7 @@ const blog = defineCollection({
   }),
 });
 
-const newsletters = defineCollection({
-  type: "content",
-  schema: z.object({
-    issue: z.number().optional(),
-  }),
-});
-
-const papers = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    subtitle: z.string().optional(),
-    authors: z.array(z.string()),
-    emails: z.array(z.string()),
-    date: z.date(),
-    tags: z.array(z.string()).optional(),
-  }),
-});
-
 export const collections = {
   blog: blog,
-  newsletters: newsletters,
   docs: docs,
-  papers: papers,
 };
